9383fd0a0c4ac193a9b755f0ba29a5c7d500cfee
Barry d'Hoine <barrydhoine@gmail.com>
2025-03-08 01:06:15 +0100

    Fix startup of container

--- aem-start.sh
+++ aem-start.sh
@@ -10,6 +10,7 @@ trap aem_stop INT TERM EXIT
 # link stdout to container output
 ln -sf /proc/1/fd/1 /opt/aem/home/var/instance/${RUNMODE}/crx-quickstart/logs/stdout.log
 
+cd /opt || exit 1
 /usr/local/bin/aem instance --instance-${RUNMODE} start
 
 # tail error log in subshell to keep container running and allow for graceful shutdown
